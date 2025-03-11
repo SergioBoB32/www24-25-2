b3d2925e92ee3e654a7ffced2e0b450139aa0312
SergioBoB32 <152491235+SergioBoB32@users.noreply.github.com>
2025-03-11 09:05:46 +0100

    Add files via upload

---
+++ 4.4.-Design/JS/java.js
@@ -0,0 +1,15 @@
+function Michelle(){
+
+    document.getElementById("activo").style.display = "none";
+    document.getElementById("inactivo").style.display = "contents";
+
+    
+}
+function Share(){
+
+    document.getElementById("inactivo").style.display = "none"
+    document.getElementById("activo").style.display = "contents"
+
+
+
+}
